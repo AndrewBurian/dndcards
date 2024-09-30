3da442a6b092133d22da4fc0f89a60d81603215a
Andrew Burian <andrew@burian.dev>
2024-09-30 11:28:37 +0930

    improved template

--- build.mjs
+++ build.mjs
@@ -1,18 +1,22 @@
 window.onload = async () => {
+    Handlebars.registerHelper('includes', function (value, target, options) {
+        if (!Array.isArray(value)) return
+        if (value.includes(target)) {
+            return options.fn(this)
+        }
+    });
+    Handlebars.registerHelper('eq', function (value, target, options) {
+        if (value == target) return options.fn(this)
+    });
     let rawTmpl = await fetch('./card.hbs')
     var template = Handlebars.compile(await rawTmpl.text())
     let data = await fetch('./cards.json');
     let cards = await data.json();
-    console.log(`Got cards`)
-    console.dir(cards)
     let printArea = document.getElementById('cards')
     cards.forEach(card => {
-        console.log(`compiling card ${card.title}`)
         let temp = document.createElement('template')
         let html = template(card)
-        console.log(html)
         temp.innerHTML = html
-        console.dir(temp)
         printArea.appendChild(temp.content.childNodes[0])
     })
 }
